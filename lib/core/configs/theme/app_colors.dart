0b2c0710d13ac9f9e25a468ae0ab80fcf96ba053
Mathis GILLARD <mathis.gillard@gmail.com>
2024-11-27 18:43:02 +0100

    :art: ADD : new project color (for error)

--- lib/core/configs/theme/app_colors.dart
+++ lib/core/configs/theme/app_colors.dart
@@ -2,5 +2,6 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   static const primary = Color(0xffFF9500);
+  static const error = Color(0xffFF0000);
   static const lightBackground = Colors.white;
 }
