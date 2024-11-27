f93c4eae12e36d8d3e003d869d0b10d6457afcf8
Mathis GILLARD <mathis.gillard@gmail.com>
2024-11-27 18:42:41 +0100

    :zap: ADD : Simple App bar (with go back option)

--- lib/core/configs/theme/app_theme.dart
+++ lib/core/configs/theme/app_theme.dart
@@ -3,35 +3,36 @@ import 'package:flutter/material.dart';
 
 class AppTheme {
   static final lightTheme = ThemeData(
-    useMaterial3: true,
-    primaryColor: AppColors.primary,
-    brightness: Brightness.light,
-    indicatorColor: AppColors.primary,
-    navigationBarTheme: const NavigationBarThemeData(
-      backgroundColor: Colors.white,
-      surfaceTintColor: Colors.white,
+      useMaterial3: true,
+      primaryColor: AppColors.primary,
+      scaffoldBackgroundColor: AppColors.lightBackground,
+      brightness: Brightness.light,
       indicatorColor: AppColors.primary,
-      labelBehavior: NavigationDestinationLabelBehavior.alwaysHide,
-    ),
-    progressIndicatorTheme: const ProgressIndicatorThemeData(
-      color: AppColors.primary,
-    ),
-    elevatedButtonTheme: ElevatedButtonThemeData(
-      style: ElevatedButton.styleFrom(
-        backgroundColor: AppColors.primary, // Couleur de fond
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(100.0), // Ajuste le rayon ici
-        ),
-        foregroundColor: Colors.white,
-        textStyle: const TextStyle(
-          fontSize: 24.0, // Ajuste la taille de texte ici
-          fontWeight: FontWeight.bold, // Ajuste le poids de texte ici
-          color: Colors.white, // Couleur du texte
-        ),
-        fixedSize: const Size(214, 52),
+      navigationBarTheme: const NavigationBarThemeData(
+        backgroundColor: Colors.white,
+        surfaceTintColor: Colors.white,
+        indicatorColor: AppColors.primary,
+        labelBehavior: NavigationDestinationLabelBehavior.alwaysHide,
       ),
-    ),
-  );
+      progressIndicatorTheme: const ProgressIndicatorThemeData(
+        color: AppColors.primary,
+      ),
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: AppColors.primary, // Couleur de fond
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(100.0), // Ajuste le rayon ici
+          ),
+          foregroundColor: Colors.white,
+          textStyle: const TextStyle(
+            fontSize: 24.0, // Ajuste la taille de texte ici
+            fontWeight: FontWeight.bold, // Ajuste le poids de texte ici
+            color: Colors.white, // Couleur du texte
+          ),
+          fixedSize: const Size(214, 52),
+        ),
+      ),
+      appBarTheme: const AppBarTheme(color: AppColors.lightBackground));
 } 
 
 //TODO maybe install a darkTheme later
